9358978dafbbcfe4313305782a2a259d5b9ddd6f
wj7788 <wj7788@arcsoft.com.cn>
2020-08-10 18:50:06 +0800

    feat:新增comment、config的model

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -6,8 +6,12 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'wj522059',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
